519386124c7e687e4872c6944fe32703ab21917e
sijie.sun <sijie.sun@smartx.com>
2025-01-10 23:02:35 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# kcp-sys
+
+Safe bindings to the [kcp](https://github.com/skywind3000/kcp) transport protocol library.
---
+++ build.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+fn main() {
+    println!("cargo:rustc-link-lib=kcp");
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let fulldir = Path::new(&dir).join("kcp");
+
+    let mut config = cc::Build::new();
+    config.include(fulldir.clone());
+    config.file(fulldir.join("ikcp.c"));
+    config.opt_level(3);
+    config.warnings(false);
+    config.compile("libkcp.a");
+    println!("cargo:rustc-link-search=native={}", fulldir.display());
+
+    println!("cargo:rerun-if-changed=kcp/ikcp.h");
+    println!("cargo:rerun-if-changed=kcp/ikcp.c");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .allowlist_function("ikcp_.*")
+        .opaque_type("ikcpcb")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
